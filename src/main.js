ebcc4828bdfcc429e59c5c13a5459a06d33831a9
Carol Marulanda <clmarulandac@gmail.com>
2021-03-25 09:30:19 -0500

    data

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data); 
+
+let namePersonajes= data.pokemon;
+
+for (let i =0; i<namePersonajes.length; i++){
+    console.log(namePersonajes[i].name);
+}
