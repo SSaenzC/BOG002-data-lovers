619a4f625abb696e353b13df80bd391d3403da85
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-03-29 09:40:47 -0500

    main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,22 @@ import data from './data/pokemon/pokemon.js';
 let personajes = data.pokemon;
 
 for(let i = 0; i < personajes.length; i++){
-    let nombrePersonajes = personajes[i].name;
+    //contenedores
+    let contenedorTarjeta = document.getElementById("profile");
+    let contenedorNumero = document.createElement("p");
+    console.log("ola ke ace");
+    
+    /*iteración sobre la información traida
+    let nombrePersonaje = personajes[i].name;
     let numeroPersonaje = personajes[i].num;
+    let imagenPersonaje = personajes[i].img;*/
+
+    
+
+    contenedorNumero.innerHTML=personajes[1].num;
+    return contenedorNumero
+    
 }
+
+
 
