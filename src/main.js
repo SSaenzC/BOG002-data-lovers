d74184400fd3beca01cf0a77e24fad7cdb1f2094
Carol Marulanda <clmarulandac@gmail.com>
2021-03-29 09:27:49 -0500

    main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,33 @@ import data from './data/pokemon/pokemon.js';
 let personajes = data.pokemon;
 
 for(let i = 0; i < personajes.length; i++){
+
     let nombrePersonajes = personajes[i].name;
     let numeroPersonaje = personajes[i].num;
+    let imagenPersonajes = personajes[i].img;
+
+   let contenedorpersonajes = document.getElementById("pokemons");
+
+   let nuevoElemento = document.createElement("div");
+   let nuevaImagen = document.createElement("img");
+   let nuevoNombre = document.createElement("h1");
+   let nuevoNum = document.createElement("h2")
+
+   nuevoElemento.className = "tarjeta";
+
+   nuevaImagen.src=personajes[i].img;
+   nuevoNombre.innerHTML=personajes[i].name;
+   nuevoNum.innerHTML=personajes[i].num;
+
+   nuevoElemento.appendChild(nuevaImagen);
+   nuevoElemento.appendChild(nuevoNombre);
+   nuevoElemento.appendChild(nuevoNum);
+   contenedorpersonajes.appendChild(nuevoElemento);
+
+
+
+
+
 }
+  
 
