602458006603b272a0d90c77de57492fd67db690
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-04-05 09:22:02 -0500

    modificación main.js

--- src/main.js
+++ src/main.js
@@ -3,37 +3,33 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+//document.getElementById("homeNav").addEventListener("click", display());
+
+/*function display(shown, hidden){
+    document.getElementById("shown").style.display='block';
+    document.getElementById("hidden").style.display='none';
+    console.log('I am working');
+}*/
+
+
+//Se alojan los datos que estan en el archivo pokemon.js en esta variable
 let personajes = data.pokemon;
 
+
 for(let i = 0; i < personajes.length; i++){
-<<<<<<< HEAD
-    //contenedores
-    let contenedorTarjeta = document.getElementById("profile");
-    let contenedorNumero = document.createElement("p");
-    console.log("ola ke ace");
-    
-    /*iteración sobre la información traida
-    let nombrePersonaje = personajes[i].name;
-    let numeroPersonaje = personajes[i].num;
-    let imagenPersonaje = personajes[i].img;*/
 
-    
-
-    contenedorNumero.innerHTML=personajes[1].num;
-    return contenedorNumero
-    
-=======
-
+    //Se crean las variables para que se aloje cada dato necesitado en específico
     let nombrePersonajes = personajes[i].name;
     let numeroPersonaje = personajes[i].num;
     let imagenPersonajes = personajes[i].img;
 
-   let contenedorpersonajes = document.getElementById("pokemons");
+   //let contenedorpersonajes = document.getElementById("pokemons");
+   let contenedorpersonajes = document.getElementById("contenedor");
 
    let nuevoElemento = document.createElement("div");
    let nuevaImagen = document.createElement("img");
-   let nuevoNombre = document.createElement("h1");
-   let nuevoNum = document.createElement("h2")
+   let nuevoNombre = document.createElement("button");
+   let nuevoNum = document.createElement("p")
 
    nuevoElemento.className = "tarjeta";
 
@@ -45,14 +41,5 @@ for(let i = 0; i < personajes.length; i++){
    nuevoElemento.appendChild(nuevoNombre);
    nuevoElemento.appendChild(nuevoNum);
    contenedorpersonajes.appendChild(nuevoElemento);
-
-
-
-
-
->>>>>>> d74184400fd3beca01cf0a77e24fad7cdb1f2094
 }
-  
-
-
 
