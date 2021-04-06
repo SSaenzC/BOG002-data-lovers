3c9b721d61284efec6601b43d6a301de3125d18f
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-04-06 17:50:39 -0500

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,43 +3,66 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-//document.getElementById("homeNav").addEventListener("click", display());
-
-/*function display(shown, hidden){
-    document.getElementById("shown").style.display='block';
-    document.getElementById("hidden").style.display='none';
-    console.log('I am working');
-}*/
-
-
-//Se alojan los datos que estan en el archivo pokemon.js en esta variable
-let personajes = data.pokemon;
+let personajes = data.pokemon; 
 
 
 for(let i = 0; i < personajes.length; i++){
-
-    //Se crean las variables para que se aloje cada dato necesitado en específico
     let nombrePersonajes = personajes[i].name;
     let numeroPersonaje = personajes[i].num;
     let imagenPersonajes = personajes[i].img;
 
-   //let contenedorpersonajes = document.getElementById("pokemons");
-   let contenedorpersonajes = document.getElementById("contenedor");
+    let contenedorpersonajes = document.getElementById("contenedor");
 
-   let nuevoElemento = document.createElement("div");
-   let nuevaImagen = document.createElement("img");
-   let nuevoNombre = document.createElement("button");
-   let nuevoNum = document.createElement("p")
+    let nuevoElemento = document.createElement("div");
+    let nuevaImagen = document.createElement("img");
+    let nuevoNombre = document.createElement("button");
+    let nuevoNum = document.createElement("p")
 
-   nuevoElemento.className = "tarjeta";
+    nuevoElemento.className = "tarjeta";
 
-   nuevaImagen.src=personajes[i].img;
-   nuevoNombre.innerHTML=personajes[i].name;
-   nuevoNum.innerHTML=personajes[i].num;
+    nuevaImagen.src=personajes[i].img;
+    nuevoNombre.innerHTML=personajes[i].name;
+    nuevoNum.innerHTML=personajes[i].num;
 
-   nuevoElemento.appendChild(nuevaImagen);
-   nuevoElemento.appendChild(nuevoNombre);
-   nuevoElemento.appendChild(nuevoNum);
-   contenedorpersonajes.appendChild(nuevoElemento);
+    nuevoElemento.appendChild(nuevaImagen);
+    nuevoElemento.appendChild(nuevoNombre);
+    nuevoElemento.appendChild(nuevoNum);
+    contenedorpersonajes.appendChild(nuevoElemento);
 }
+
+//Ventana modal
+document.getElementById("myBtn").addEventListener("click", showModal);
+
+function showModal(){
+    modal.style.display = "block";
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+/* When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}*/
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+  
+
 
