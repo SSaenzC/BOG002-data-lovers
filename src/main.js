a112d0b0014ed1b5e1994aa9ec264b1e4e35f6c5
Carol Marulanda <clmarulandac@gmail.com>
2021-04-07 13:28:00 -0500

    cambios generales

--- src/main.js
+++ src/main.js
@@ -9,25 +9,73 @@ for(let i = 0; i < personajes.length; i++){
     let nombrePersonajes = personajes[i].name;
     let numeroPersonaje = personajes[i].num;
     let imagenPersonajes = personajes[i].img;
+    let about = personajes[i].about;
+    let tipoPersonajes = personajes[i].type[0];
 
+    let todosJuntos = [];
+
+    todosJuntos.push(tipoPersonajes);
+
+    let set = [...new Set (todosJuntos)];
+
+
+    
+
+
+
+    //console.log(personajes[i].type);//
+    
     let contenedorpersonajes = document.getElementById("contenedor");
 
+    let nuevoNum = document.createElement("p")
     let nuevoElemento = document.createElement("div");
     let nuevaImagen = document.createElement("img");
     let nuevoNombre = document.createElement("button");
-    let nuevoNum = document.createElement("p")
+    let nuevoAbout =  document.createElement("p");
+
+        nuevoNombre.style.width= "30%";
+        nuevoNombre.style.display="flex";
+        nuevoNombre.style.alignItems="center";
+        nuevaImagen.style.background="rgb(243, 246, 247)";
+        nuevaImagen.style.border="outset";
+        nuevoAbout.style.float="right";
+
 
     nuevoElemento.className = "tarjeta";
 
+    nuevoNum.innerHTML=personajes[i].num;
     nuevaImagen.src=personajes[i].img;
     nuevoNombre.innerHTML=personajes[i].name;
-    nuevoNum.innerHTML=personajes[i].num;
-
+    nuevoAbout.innerHTML=personajes[i].about;
+    
+    nuevoElemento.appendChild(nuevoNum);
     nuevoElemento.appendChild(nuevaImagen);
     nuevoElemento.appendChild(nuevoNombre);
-    nuevoElemento.appendChild(nuevoNum);
+    nuevoElemento.appendChild( nuevoAbout);
+    
     contenedorpersonajes.appendChild(nuevoElemento);
+
+    let contenedorTipos = document.getElementById("type");
+
+    let nuevoType = document.createElement("div");
+    let nuevoTipos = document.createElement("h1")
+
+    nuevoType.className = "tipos";
+
+    nuevoTipos.innerHTML = set;
+
+
+
+    nuevoType.appendChild(nuevoTipos);
+
+    contenedorTipos.appendChild(nuevoType);
+
+
+
+    
 }
+
+
   
 
 
