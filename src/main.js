39e3e0a56e6e30903c5b1ac979a530d25f2fe9a3
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-04-07 00:36:22 -0500

    funcion botones nombre pokemon

--- src/main.js
+++ src/main.js
@@ -3,66 +3,61 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-let personajes = data.pokemon; 
+let personajes = data.pokemon;
 
+crearTarjetas(personajes)
 
-for(let i = 0; i < personajes.length; i++){
-    let nombrePersonajes = personajes[i].name;
-    let numeroPersonaje = personajes[i].num;
-    let imagenPersonajes = personajes[i].img;
+function crearTarjetas(datosPersonajes){ 
+  for(let i = 0; i < personajes.length; i++){
+      let nombrePersonajes = personajes[i].name;
+      let numeroPersonaje = personajes[i].num;
+      let imagenPersonajes = personajes[i].img;
 
-    let contenedorpersonajes = document.getElementById("contenedor");
+      let contenedorpersonajes = document.getElementById("contenedor");
 
-    let nuevoElemento = document.createElement("div");
-    let nuevaImagen = document.createElement("img");
-    let nuevoNombre = document.createElement("button");
-    let nuevoNum = document.createElement("p")
+      let nuevoElemento = document.createElement("div");
+      let nuevaImagen = document.createElement("img");
+      let nuevoNombre = document.createElement("button");
+      let nuevoNum = document.createElement("p")
 
-    nuevoElemento.className = "tarjeta";
+      nuevoNombre.className = 'nombrePokemon';
 
-    nuevaImagen.src=personajes[i].img;
-    nuevoNombre.innerHTML=personajes[i].name;
-    nuevoNum.innerHTML=personajes[i].num;
+      nuevoElemento.className = "tarjeta";
 
-    nuevoElemento.appendChild(nuevaImagen);
-    nuevoElemento.appendChild(nuevoNombre);
-    nuevoElemento.appendChild(nuevoNum);
-    contenedorpersonajes.appendChild(nuevoElemento);
+      nuevaImagen.src=personajes[i].img;
+      nuevoNombre.innerHTML=personajes[i].name;
+      nuevoNum.innerHTML=personajes[i].num;
+
+      nuevoElemento.appendChild(nuevaImagen);
+      nuevoElemento.appendChild(nuevoNombre);
+      nuevoElemento.appendChild(nuevoNum);
+      contenedorpersonajes.appendChild(nuevoElemento);
+
+      let botonPokemon = document.getElementsByClassName('nombrePokemon');
+      //botonPokemon[i].addEventListener('click', mostrarModal);
+      //document.getElementsByTagName('button').addEventListener('click', mostrarModal);
+      document.getElementById('myBtn').addEventListener('click', mostrarModal);
+  }
 }
-
 //Ventana modal
-document.getElementById("myBtn").addEventListener("click", showModal);
-
-function showModal(){
-    modal.style.display = "block";
+//document.getElementById('myBtn').addEventListener('click', mostrarModal);
+function mostrarModal(){
+  document.getElementById('myModal').style.display='block';
+    //for(let i = 0; i< personajes.length; i++){
+      // console.log(personaje);
+   // }
+    console.log(personajes[0]);
 }
 
-var modal = document.getElementById("myModal");
+//cerrar ventana modal
+document.getElementById('close').addEventListener('click', cerrarModal);
+function cerrarModal(){
+  document.getElementById('myModal').style.display ='none';
+  console.log('No me quiero ir señor Stark');
+}
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-/* When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}*/
-
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
-  
-
-
