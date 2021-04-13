93f133f0c16d0211a5195811ce09278db02de5fb
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-04-13 08:49:01 -0500

    info ventana modal

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import data from './data/pokemon/pokemon.js';
 
 let personajes = data.pokemon;
 
-  crearTarjetas(personajes)
+  crearTarjetas(personajes);
 
-function crearTarjetas(datosPersonajes){ 
+function crearTarjetas(){ 
   for(let i = 0; i < personajes.length; i++){
       /*let nombrePersonajes = personajes[i].name;
       let numeroPersonaje = personajes[i].num;
@@ -20,7 +20,7 @@ function crearTarjetas(datosPersonajes){
       let nuevaImagen = document.createElement('img');
       let nuevoNombre = document.createElement('button');
       let nuevoNum = document.createElement('p');
-      let nuevaDescrip = document.createElement('p');
+      
       
 
       nuevoElemento.className = 'tarjeta';
@@ -31,12 +31,11 @@ function crearTarjetas(datosPersonajes){
       nuevaImagen.src = personajes[i].img;
       nuevoNombre.innerHTML = personajes[i].name;
       nuevoNum.innerHTML = personajes[i].num;
-      nuevaDescrip.innerHTML = personajes[i].about;
+      
 
       nuevoElemento.appendChild(nuevoNum);
       nuevoElemento.appendChild(nuevaImagen);
       nuevoElemento.appendChild(nuevoNombre);
-      nuevoElemento.appendChild(nuevaDescrip);
       contenedorpersonajes.appendChild(nuevoElemento);
 
       let botonPokemon = document.getElementsByClassName('nombrePokemon');
@@ -50,10 +49,30 @@ function mostrarModal(event){
   const perfilPokemon = event.currentTarget.id;
   const encontrandoPokemon = personajes.find(elemento => elemento.num == perfilPokemon);
   const cajitaPokemon = document.createElement('div');
-  const tipoPokemon = document.createElement('div');
-
+  const imgPokemon = document.createElement('img');
+  const acercaDelPokemon = document.createElement('p');
+  const nombrePokemon = document.createElement('p');
+  const pesoPokemon = document.createElement('p');
+  const tipoPokemon = document.createElement('p');
+  const debilidadPokemon = document.createElement('p');
+  const evolucionPokemon = document.createElement('p');
+ 
   cajitaPokemon.id = 'modalDePokemon';
   cajitaPokemon.innerHTML = encontrandoPokemon.num;
+  imgPokemon.src = encontrandoPokemon.img;
+  nombrePokemon.innerHTML = encontrandoPokemon.name;
+  acercaDelPokemon.innerHTML = encontrandoPokemon.about;
+  pesoPokemon.innerHTML = encontrandoPokemon.size.weight;
+  tipoPokemon.innerHTML = encontrandoPokemon.type
+  debilidadPokemon.innerHTML = encontrandoPokemon.weaknesses;
+
+  cajitaPokemon.appendChild(imgPokemon);
+  cajitaPokemon.appendChild(nombrePokemon);
+  cajitaPokemon.appendChild(acercaDelPokemon);
+  cajitaPokemon.appendChild(pesoPokemon);
+  cajitaPokemon.appendChild(tipoPokemon);
+  cajitaPokemon.appendChild(debilidadPokemon);
+  
   
   console.log(encontrandoPokemon);
   let modal = document.getElementById('miModal');
