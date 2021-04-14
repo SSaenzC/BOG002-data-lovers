c3e116c8d515057d9bf5f74faeba543bebe377b0
Carol Marulanda <clmarulandac@gmail.com>
2021-04-13 23:13:55 -0500

    css

--- src/data.js
+++ src/data.js
@@ -1,10 +1,15 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-    return 'example';
-  };
-  
-  export const anotherExample = () => {
-    return 'OMG';
-  };
 
+/*export const example = () => {
+  return 'example';
+};
+export const anotherExample = () => {
+  return 'OMG';
+};*/
+
+export const filtrarTipo = (name, Pokemons) => {
+    return Pokemons.filter(Pokemon => Pokemon.name === name);
+  }
+  
+  //console.log(filterType);
